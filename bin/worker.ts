0e8d4111fbd8fed38303ebdc7992b9977ab5c682
Ted Cook <teodoro.cook@gmail.com>
2023-04-06 23:50:17 -0600

    Add environment undeploy job

--- bin/worker.ts
+++ bin/worker.ts
@@ -5,89 +5,131 @@ import { execSync } from 'child_process';
 import { Logger } from 'tslog';
 import * as toml from 'toml';
 import * as fs from 'fs';
+import * as dotenv from 'dotenv';
 
-const CONFIG_FILE = 'wrangler.toml';
-const LOG_LEVELS = {
-	silly: 0,
-	trace: 1,
-	debug: 2,
-	info: 3,
-	warn: 4,
-	error: 5,
-	fatal: 6
+if (fs.existsSync(`${process.cwd()}/.env`)) {
+	dotenv.config({ path: `${process.cwd()}/.env` });
+}
+
+const config = {
+	GITHUB_API_URL: 'https://api.github.com/repos',
+	GITHUB_REPOSITORY: process.env.GITHUB_REPOSITORY || null,
+	WRANGLER_CONFIG: 'wrangler.toml',
+	LOG_LEVELS: {
+		silly: 0,
+		trace: 1,
+		debug: 2,
+		info: 3,
+		warn: 4,
+		error: 5,
+		fatal: 6
+	}
 };
-const logger: Logger<ILogObj> = new Logger({ name: 'worker', minLevel: LOG_LEVELS.info });
+const logger: Logger<ILogObj> = new Logger({ name: 'worker', minLevel: config.LOG_LEVELS.info });
 
-function isGithubAction(): boolean {
-	return process.env.GITHUB_ACTIONS === 'true';
+function readConfig(configFile: string = config.WRANGLER_CONFIG) {
+	return toml.parse(fs.readFileSync(configFile, 'utf-8'));
 }
 
-function readConfig(configFile: string = CONFIG_FILE) {
-	const config = toml.parse(fs.readFileSync(configFile, 'utf-8'));
-	return config;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+//async function githubAPI(method: string, path: string, body: any = null): Promise<any[]> {
+//const headers = { Authorization: `Bearer ${config.cloudflare_api_token}` };
+//const url = `${config.cloudflare_api_url}/accounts/${config.CLOUDFLARE_ACCOUNT_ID}/${path}`;
+//logger.debug('Calling Cloudflare API:');
+//logger.debug(`  curl -X${method} ${url} -H "Authorization: ${headers.Authorization}"`);
+//const response = await fetch(url, {
+//	method,
+//	headers,
+//	body
+//});
+//if (!response.ok) {
+//	throw new Error(`Failed to fetch worker data: ${response.status}`);
+//}
+//const data = await response.json();
+//return [data.result].flat();
+//}
+
+function cloudflareWorkerName(prefix: string): string {
+	const { name } = readConfig(config.WRANGLER_CONFIG);
+	if (prefix != '' && prefix != 'main' && prefix != 'master') {
+		return `${prefix}-${name}`;
+	} else {
+		return name;
+	}
 }
 
-function deployCloudflareWorker(branch: string): string | null {
-	logger.debug('Entering deploy command handler');
-	const config = readConfig(CONFIG_FILE);
-	const workerName = `${branch}-${config.name}`;
-	const command = `npm exec wrangler publish -- --name ${workerName}`;
+function deployCloudflareWorker(name: string) {
+	const command = `npm exec wrangler publish -- --name ${name.trim()}`;
 	try {
-		logger.debug(`Running command: ${command}`);
-		const outBuffer = execSync(command);
-		const workerId = outBuffer
-			.toString()
+		logger.debug(`Executing ${command}`);
+		const output = execSync(`${command} 2>&1`).toString();
+		const deploymentId = output
 			.split('\n')
 			.filter((line) => line.match(/.*Deployment ID:/))[0]
 			.split(':')[1]
 			.trim();
-		logger.info(`worker deployed successfully with id ${workerId}`);
-		logger.debug('Exiting deploy command handler');
-		return workerId;
+		const workerUrl = output
+			.split('\n')
+			.filter((line) => line.match(/.*workers.dev$/))[0]
+			.trim();
+		logger.info(`Worker deployment ${deploymentId} successful`);
+		logger.info(`Worker deployed at ${workerUrl}`);
+		return { deploymentId, workerUrl };
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	} catch (error: any) {
 		const { status, outBuffer, errBuffer } = error;
 		const stderr = (errBuffer && errBuffer.toString()) || 'unknown error';
 		const stdout = (outBuffer && outBuffer.toString()) || 'no output';
-		logger.error(`worker deployment failed: ${status || 'interrupted'}`);
+		logger.error(`worker deployment failed wiith code ${status || 'interrupted'}`);
 		logger.error(`stdout: ${stdout}`);
 		logger.error(`stderr: ${stderr}`);
-		return null;
+		throw new Error(`Failed to deploy cloudflare Worker ${name}`);
 	}
 }
 
-function deploy(branch: string): void {
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	const workerId = deployCloudflareWorker(branch);
+async function deploy(name: string) {
+	logger.debug('Entering deploy command handler');
+	const { workerUrl } = deployCloudflareWorker(name);
+	logger.debug(`Worker deployed at ${workerUrl}`);
+	logger.debug('Exiting deploy command handler');
 }
 
-function removeCloudflareWorker(branch: string): string | null {
-	logger.debug('Entering remove command handler');
-	const config = readConfig(CONFIG_FILE);
-	const workerName = `${branch}-${config.name}`;
-	const command = `npm exec wrangler delete -- --name ${workerName}`;
+function removeCloudflareWorker(name: string): void {
+	const command = `npm exec wrangler delete -- --name ${name}`;
 	logger.debug(command);
 	try {
 		execSync(command);
 	} catch {} // eslint-disable-line no-empty
-	logger.info(`worker ${workerName} removed successfully`);
+	logger.info(`worker ${name} removed successfully`);
+}
+
+function remove(name: string): void {
+	logger.debug('Entering remove command handler');
+	removeCloudflareWorker(name);
 	logger.debug('Exiting remove command handler');
-	return null;
 }
 
-function remove(branch: string): void {
-	removeCloudflareWorker(branch);
+// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
+async function handle(program: Command, fn: (x: string) => void, args: any) {
+	const isVerbose = program.opts()['verbose'];
+	const isQuiet = program.opts()['quiet'];
+	const isGithubAction = process.env.GITHUB_ACTIONS === 'true';
+	if (isVerbose) logger.settings.minLevel = config.LOG_LEVELS.debug;
+	if (isQuiet) logger.settings.minLevel = config.LOG_LEVELS.fatal;
+	if (isGithubAction) logger.settings.minLevel = config.LOG_LEVELS.fatal;
+	logger.debug(`Running action handler with args ${JSON.stringify(args)}`);
+	const workerName = process.env.WORKER_NAME
+		? cloudflareWorkerName(process.env.WORKER_NAME)
+		: cloudflareWorkerName(program.opts()['prefix']);
+	logger.debug(`Deploying cloudflare worker ${workerName}`);
+	fn(workerName);
 }
 
-async function handle<T>(program: Command, fn: (x: T) => void, arg: T) {
-	logger.debug('Entering generic command handler');
-	const verbose = program.opts()['verbose'];
-	const quiet = program.opts()['quiet'];
-	if (verbose) logger.settings.minLevel = LOG_LEVELS.debug;
-	if (quiet) logger.settings.minLevel = LOG_LEVELS.fatal;
-	if (isGithubAction()) logger.settings.minLevel = LOG_LEVELS.fatal;
-	fn(arg);
-	logger.debug('Exiting generic command handler');
+function checkConfig() {
+	if (!config.GITHUB_REPOSITORY) {
+		logger.fatal('GITHUB_REPOSITORY environment variable is not set');
+		process.exit(1);
+	}
 }
 
 function main() {
@@ -97,16 +139,12 @@ function main() {
 		.version('0.0.1', '--version', 'output the current version')
 		.description('worker deployment tool')
 		.option('-v, --verbose', 'verbose output', false)
-		.option('-q, --quiet', 'quiet output (overrides verbose)', false);
-	program
-		.command('deploy')
-		.option('-b, --branch <branch>', 'branch name')
-		.action((options) => handle(program, deploy, options.branch));
-	program
-		.command('remove')
-		.option('-b, --branch <branch>', 'branch name')
-		.action((options) => handle(program, remove, options.branch));
+		.option('-q, --quiet', 'quiet output (overrides verbose)', false)
+		.option('-p, --prefix', 'deployment name prefix', 'test');
+	program.command('deploy').action((options) => handle(program, deploy, options));
+	program.command('remove').action((options) => handle(program, remove, options));
 	program.parse(process.argv);
 }
 
+checkConfig();
 main();
