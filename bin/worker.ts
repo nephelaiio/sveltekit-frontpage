c45c3367395df2bd68cedca3a3d7868b96a9f411
Ted Cook <teodoro.cook@gmail.com>
2023-04-08 19:26:57 -0600

    Drop debug statements

--- bin/worker.ts
+++ bin/worker.ts
@@ -100,11 +100,8 @@ async function handle(program: Command, fn: (x: string) => void, args: any) {
 	if (isQuiet) logger.settings.minLevel = LOG_LEVELS.fatal;
 	if (isGithubAction) logger.settings.minLevel = LOG_LEVELS.fatal;
 	logger.debug(`Running action handler with args ${JSON.stringify(args)}`);
-	console.log(JSON.stringify(program.opts()));
 	const prefix = program.opts()['prefix'];
-	console.log(`Deploying worker with prefix ${prefix}`);
 	const workerName = cloudflareWorkerName(prefix);
-	console.log(`Deploying cloudflare worker ${workerName}`);
 	logger.debug(`Deploying cloudflare worker ${workerName}`);
 	fn(workerName);
 }
